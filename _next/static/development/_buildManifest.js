7a5751e227f2e9e01c1fef7e1cd0ec5a7fa5bf46
hshoff <hi@hshoff.com>
2020-10-09 12:55:47 -0400

    Deploy commit

---
+++ _next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/gallery":["static\u002Fchunks\u002Fpages\u002Fgallery.js"],"/streamgraph":["static\u002Fchunks\u002Fpages\u002Fstreamgraph.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fgallery","\u002Fstreamgraph"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
